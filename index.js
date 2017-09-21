9d5d14128d8cd0c959bdc64193f5046e31d1dee4
Rémy Jeancolas <remy.jeancolas@ssense.com>
2017-09-21 13:24:57 -0400

    0.1.24

--- index.js
+++
@@ -1,8 +0,0 @@
-const http = require('http');
-const server = http.createServer((req, res) => {
-    console.log(req, res);
-});
-
-server.listen(8080, () => {
-    console.log('server listening');
-});
