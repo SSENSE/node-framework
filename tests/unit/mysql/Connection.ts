176826d82f5dc97ffdea36e75dce44a627a127ae
Rémy Jeancolas <remy.jeancolas@ssense.com>
2018-02-07 15:26:21 -0500

    feat: Add runWithLockTables() method in MySQL connection helper

--- tests/unit/mysql/Connection.ts
+++ tests/unit/mysql/Connection.ts
@@ -186,4 +186,108 @@ describe('MysqlConnection', () => {
             expect(result).to.equal('Baz');
         });
     });
+
+    describe('runWithLockTables()', () => {
+        it('should throw an error if locks param is not a MysqlLockTableOption[]', async () => {
+            sandbox.stub(mysql, <any> 'createPool').returns(pool);
+            sandbox.stub(pool, <any> 'getConnection').callsFake((cb: (err: Error, connection: any) => void) => {
+                cb(null, poolConnection);
+            });
+            const releaseStub = sandbox.stub(poolConnection, <any> 'release');
+            const queryStub = sandbox.stub(poolConnection, <any> 'query').callsFake(
+                (sql: string, params: any[], cb: (err: Error, result: any) => void) => {
+                    cb(new Error('Foo'), null);
+                }
+            );
+            const stub = sandbox.stub();
+            const connection = new MysqlConnection({host: 'a', database: 'b'});
+            let error: string = null;
+            try {
+                await connection.runWithLockTables([], stub);
+            } catch (e) {
+                error = e.message;
+            }
+            expect(stub.callCount).to.equal(0);
+            expect(releaseStub.callCount).to.equal(0);
+            expect(queryStub.callCount).to.equal(0);
+            expect(error).to.equal('locks must be a MysqlLockTableOption[]');
+        });
+
+        it('should fail and not rollback the transaction if not started when the callback function throws an error', async () => {
+            sandbox.stub(mysql, <any> 'createPool').returns(pool);
+            sandbox.stub(pool, <any> 'getConnection').callsFake((cb: (err: Error, connection: any) => void) => {
+                cb(null, poolConnection);
+            });
+            const releaseStub = sandbox.stub(poolConnection, <any> 'release');
+            const queryStub = sandbox.stub(poolConnection, <any> 'query').callsFake(
+                (sql: string, params: any[], cb: (err: Error, result: any) => void) => {
+                    cb(new Error('Foo'), null);
+                }
+            );
+            const connection = new MysqlConnection({host: 'a', database: 'b'});
+            let error: string = null;
+            try {
+                await connection.runWithLockTables([{name: 'foo', mode: 'WRITE'}], async () => {
+                    return 'Baz';
+                });
+            } catch (e) {
+                error = e.message;
+            }
+            expect(releaseStub.callCount).to.equal(1);
+            expect(queryStub.callCount).to.equal(1);
+            expect(queryStub.lastCall.args[0]).to.equal('SET autocommit=0;');
+            expect(error).to.equal('Foo');
+        });
+
+        it('should fail, rollback the transaction and unlock tables if started when the callback function throws an error', async () => {
+            sandbox.stub(mysql, <any> 'createPool').returns(pool);
+            sandbox.stub(pool, <any> 'getConnection').callsFake((cb: (err: Error, connection: any) => void) => {
+                cb(null, poolConnection);
+            });
+            const releaseStub = sandbox.stub(poolConnection, <any> 'release');
+            const queryStub = sandbox.stub(poolConnection, <any> 'query').callsFake(
+                (sql: string, params: any[], cb: (err: Error, result: any) => void) => {
+                    cb(null, 'result');
+                }
+            );
+            const connection = new MysqlConnection({host: 'a', database: 'b'});
+            let error: string = null;
+            try {
+                await connection.runWithLockTables([{name: 'Foo', mode: 'WRITE'}, {name: 'Bar', mode: 'READ'}], async (transaction) => {
+                    await transaction.query('FOO');
+                    throw new Error('Bar');
+                });
+            } catch (e) {
+                error = e.message;
+            }
+            expect(releaseStub.callCount).to.equal(1);
+            expect(queryStub.callCount).to.equal(5);
+            expect(queryStub.secondCall.args[0]).to.equal('LOCK TABLES Foo WRITE, Bar READ;');
+            expect(queryStub.getCall(3).args[0]).to.equal('ROLLBACK;');
+            expect(queryStub.lastCall.args[0]).to.equal('UNLOCK TABLES;');
+            expect(error).to.equal('Bar');
+        });
+
+        it('should commit the transaction, unlock the tables and return result when the callback function succeeds', async () => {
+            sandbox.stub(mysql, <any> 'createPool').returns(pool);
+            sandbox.stub(pool, <any> 'getConnection').callsFake((cb: (err: Error, connection: any) => void) => {
+                cb(null, poolConnection);
+            });
+            const releaseStub = sandbox.stub(poolConnection, <any> 'release');
+            const queryStub = sandbox.stub(poolConnection, <any> 'query').callsFake(
+                (sql: string, params: any[], cb: (err: Error, result: any) => void) => {
+                    cb(null, 'result');
+                }
+            );
+            const connection = new MysqlConnection({host: 'a', database: 'b'});
+            const result = await connection.runWithLockTables([{name: 'Foo', mode: 'WRITE'}], async () => {
+                return 'Baz';
+            });
+            expect(releaseStub.callCount).to.equal(1);
+            expect(queryStub.callCount).to.equal(4);
+            expect(queryStub.thirdCall.args[0]).to.equal('COMMIT;');
+            expect(queryStub.lastCall.args[0]).to.equal('UNLOCK TABLES;');
+            expect(result).to.equal('Baz');
+        });
+    });
 });
